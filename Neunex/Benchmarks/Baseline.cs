973e93aef82d8ddbe5b5acfdfaadc19b4e4164c6
Anthony Donnelly <44259387+Amzo@users.noreply.github.com>
2023-01-16 09:06:52 +0000

    Setup baseline benchmark and split benchmarks

--- Neunex/Benchmarks/Baseline.cs
+++ Neunex/Benchmarks/Baseline.cs
@@ -1,19 +1,82 @@
-﻿using Neunex.DataSetGenerator;
+﻿using Keras;
+using Keras.Layers;
+using Keras.Models;
+using Neunex.DataSetGenerator;
 using Neunex.LabelEncoding;
+using Numpy;
+using System;
 using System.Collections.Generic;
 
 namespace Neunex.Benchmarks
 {
     public class Baseline
     {
-        public void cifar10Benchmark()
+        public static void cifar10Benchmark(int epoch, int batch)
         {
-          
+            var ((x_train, y_train), (x_test, y_test)) = DataGenerate.Cifar10();
+            var test = DataGenerate.Split(10, x_train, y_train);
+
+            var encoded = Keras.Utils.Util.ToCategorical(y_train, 10);
+            var encoded2  = Keras.Utils.Util.ToCategorical(y_test, 10);
+
+            Shape input_shape = (32,32,3);
+            var model = new Sequential();
+            model.Add(new Conv2D(32, kernel_size: (3, 3).ToTuple(),
+                                    activation: "relu",
+                                    input_shape: input_shape));
+            model.Add(new Conv2D(64, (3, 3).ToTuple(), activation: "relu"));
+            model.Add(new MaxPooling2D(pool_size: (2, 2).ToTuple()));
+            model.Add(new Dropout(0.25));
+            model.Add(new Flatten());
+            model.Add(new Dense(128, activation: "relu"));
+            model.Add(new Dropout(0.5));
+            model.Add(new Dense(10, activation: "softmax"));
+
+            model.Compile(optimizer: "adam", loss: "categorical_crossentropy", metrics: new string[] { "accuracy" });
+            model.Fit(x_train, encoded, batch_size: batch, epochs: epoch, verbose: 1);
+            var score = model.Evaluate(x_test, encoded2, verbose: 0);
+            Console.WriteLine($"Test loss: {score[0]}");
+            Console.WriteLine($"Test accuracy: {score[1]}");
         }
 
-        public void NeunexBench()
+        public static void NeunexBenchCifarSplit(int epoch, int batchsize, int splits)
         {
-            /*var labels = new Dictionary<int, string>() {
+            var ((x_train, y_train), (x_test, y_test)) = DataGenerate.Cifar10();
+            var (x_split, y_split) = DataGenerate.Split(10, x_train, y_train);
+
+            var encoded2 = Keras.Utils.Util.ToCategorical(y_test, 10);
+
+            Shape input_shape = (32, 32, 3);
+            var model = new Sequential();
+            model.Add(new Conv2D(32, kernel_size: (3, 3).ToTuple(),
+                                    activation: "relu",
+                                    input_shape: input_shape));
+            model.Add(new Conv2D(64, (3, 3).ToTuple(), activation: "relu"));
+            model.Add(new MaxPooling2D(pool_size: (2, 2).ToTuple()));
+            model.Add(new Dropout(0.25));
+            model.Add(new Flatten());
+            model.Add(new Dense(128, activation: "relu"));
+            model.Add(new Dropout(0.5));
+            model.Add(new Dense(10, activation: "softmax"));
+
+            for (int i = 0; i < x_split.Count; i++)
+            {
+                var weights = model.GetWeights();
+                var encoded = Keras.Utils.Util.ToCategorical(y_split[i], 10);
+                model.Compile(optimizer: "adam", loss: "categorical_crossentropy", metrics: new string[] { "accuracy" });
+                model.Fit(x_split[i], encoded, shuffle: true, batch_size: batchsize, epochs: epoch, verbose: 0);
+
+                var score = model.Evaluate(x_test, encoded2, verbose: 0);
+                model.SetWeights(weights);
+                Console.WriteLine($"MiniLearner {i} scores:");
+                Console.WriteLine($"Test loss: {score[0]}");
+                Console.WriteLine($"Test accuracy: {score[1]}");
+            }
+        }
+
+        public void NeunexBenchGeneratedData()
+        {
+            var labels = new Dictionary<int, string>() {
                 {1, "Cat"},
                 {2, "Dog"},
                 {3, "horse" },
@@ -36,14 +99,12 @@ namespace Neunex.Benchmarks
             model.Add(new Dense(10, activation: "relu"));
             model.Add(new Dense(5, activation: "softmax"));
 
-            //np.arr
-            //Compile and train
             model.Compile(optimizer: "adam", loss: "categorical_crossentropy", metrics: new string[] { "accuracy" });
             model.Fit(m, z, batch_size: 512, epochs: 1000, verbose: 1);
 
-            //var score = model.Evaluate(m_test, z_test, verbose: 0);
-            //Console.WriteLine($"Test loss: {score[0]}");
-            //Console.WriteLine($"Test accuracy: {score[1]}");*/
+            var score = model.Evaluate(m_test, z_test, verbose: 0);
+            Console.WriteLine($"Test loss: {score[0]}");
+            Console.WriteLine($"Test accuracy: {score[1]}");
         }
     }
 }
--- Neunex/Program.cs
+++ Neunex/Program.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System;
 using Keras;
 using Keras.Utils;
+using Neunex.LabelEncoding;
+using Numpy;
+using Neunex.Benchmarks;
 
 namespace Neunex
 {
@@ -12,62 +15,14 @@ namespace Neunex
     {
         static void Main(string[] args)
         {
-            var labels = new Dictionary<int, string>() {
-                {1, "Cat"},
-                {2, "Dog"},
-                {3, "horse" },
-                {4, "cow" },
-                {5, "duck" }};
+            var watch = new System.Diagnostics.Stopwatch();
 
+            watch.Start();
+            Baseline.NeunexBenchCifarSplit(50, 2048, 10);
+            //Baseline.cifar10Benchmark(50, 2048);
+            watch.Stop();
 
-            // mini learner data gen for layers >= 2 will be internal to minilearner
-            // private static
-            /*
-            var encodedResults = LabelEncoder.OneHotEncode(labels);
-            (var x, var y) = DataGenerate.Generate(50000, encodedResults);
-            (var x_test, var y_test) = DataGenerate.Generate(50000, encodedResults);
-            
-            var m = np.array(x).reshape(x.Length / 5, 5);
-            var z = np.array(y).reshape(y.Length / 5, 5);
-            var m_test = np.array(x_test).reshape(x_test.Length / 5, 5);
-            var z_test = np.array(y_test).reshape(y_test.Length / 5, 5);*/
-
-            // cifar10 data splitting code for input layer
-            var ((x_train, y_train), (x_test, y_test)) = DataGenerate.Cifar10();
-            var test = DataGenerate.Split(10, x_train, y_train);
-
-            (var train, var y) = DataGenerate.Split(10, x_train, y_train);
-            var encoded = Keras.Utils.Util.ToCategorical(y_train, 10);
-            var encoded2  = Keras.Utils.Util.ToCategorical(y_test, 10);
-
-            Shape input_shape = (32,32,3);
-            var model = new Sequential();
-            model.Add(new Conv2D(32, kernel_size: (3, 3).ToTuple(),
-                                    activation: "relu",
-                                    input_shape: input_shape));
-            model.Add(new Conv2D(64, (3, 3).ToTuple(), activation: "relu"));
-            model.Add(new MaxPooling2D(pool_size: (2, 2).ToTuple()));
-            model.Add(new Dropout(0.25));
-            model.Add(new Flatten());
-            model.Add(new Dense(128, activation: "relu"));
-            model.Add(new Dropout(0.5));
-            model.Add(new Dense(10, activation: "softmax"));
-
-            var model1 = new Sequential();
-            model1.Add(new Dense(64, activation: "relu", input_shape: new Keras.Shape(28,28)));
-            model1.Add(new Dense(128, activation: "relu"));
-            model1.Add(new Dense(10, activation: "softmax"));
-
-            //np.arr
-            //Compile and train
-            model.Compile(optimizer: "adam", loss: "categorical_crossentropy", metrics: new string[] { "accuracy" });
-            //model.Fit(m, z, batch_size: 512, epochs: 200, verbose: 1);
-            model.Fit(x_train, encoded, batch_size: 2048, epochs: 50, verbose: 1);
-
-            var score = model.Evaluate(x_test, encoded2, verbose: 0);
-            Console.WriteLine($"Test loss: {score[0]}");
-            Console.WriteLine($"Test accuracy: {score[1]}");
-
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
         }
     }
 }
